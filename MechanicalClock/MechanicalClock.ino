b4dac01151279333459a3503a7c5d3efb38ff089
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-05-13 21:00:53 +0700

    Update

--- MechanicalClock/MechanicalClock.ino
+++ MechanicalClock/MechanicalClock.ino
@@ -96,23 +96,22 @@ void setup()
   }
 
   pwmH.begin();                             //Start each board
-//  pwmM.begin();
+  pwmM.begin();
   pwmH.setOscillatorFrequency(27000000);    //Set the PWM oscillator frequency, used for fine calibration
-//  pwmM.setOscillatorFrequency(27000000);
+  pwmM.setOscillatorFrequency(27000000);
   pwmH.setPWMFreq(servoFrequency);          //Set the servo operating frequency
-//  pwmM.setPWMFreq(servoFrequency);
+  pwmM.setPWMFreq(servoFrequency);
 
   for (int i = 0 ; i <= 13 ; i++) //Set all of the servos to on or up (88:88 displayed)
   {
     pwmH.setPWM(i, 0, segmentHOn[i]);
     delay(10);
-//    pwmM.setPWM(i, 0, segmentMOn[i]);
-//    delay(10);
+    pwmM.setPWM(i, 0, segmentMOn[i]);
+    delay(10);
   }
 
   //Serial.begin(Baud Rate, Data Protocol, Rxd pin, Txd pin);
-  FPGA.begin(115200, SERIAL_8N1, 16, 17);
-
+  FPGA.begin(9600, SERIAL_8N1, 16, 17);
   delay(2000);
 }
 
@@ -130,45 +129,45 @@ void loop()
     Serial.println("RTC lost confidence in the DateTime!");
   }
 
-  FPGA.print("A");
+  FPGA.write("ABCDEFGHIJKLMNSEG");
+
   String RxdString = "";
   delay(500); // Too fast, Cannot read on the first try.
   while (FPGA.available()) {
     RxdString = FPGA.readString();
   }
-  
+
   if (RxdString != "") {
     Serial.println(RxdString);
-//    currentCmd = RxdString.substring(14, 17);
-//    currentData = RxdString.substring(0, 14);
-//    Serial.println(currentCmd);
-//    Serial.println(currentData);
-
-//    switch (currentCmd) {
-//      case CmdCode[2]: // RST
-//        // statements
-//        break;
-//      default:
-//        break;
-//    }
+    //    currentCmd = RxdString.substring(14, 17);
+    //    currentData = RxdString.substring(0, 14);
+    //    Serial.println(currentCmd);
+    //    Serial.println(currentData);
+    //
+    //    switch (currentCmd) {
+    //      case CmdCode[2]: // SEG
+    //        currentData = currentData.substring(10, 14);
+    //        hourTens = currentData.substring(10, 11).toInt();
+    //        hourUnits = currentData.substring(11, 12).toInt();
+    //        minuteTens = currentData.substring(12, 13).toInt();
+    //        minuteUnits = currentData.substring(13, 14).toInt();
+    //        break;
+    //      case CmdCode[1]: // SET
+    //        // statements
+    //        break;
+    //      default:
+    //        Serial.println("Invalid command by UART!");
+    //        break;
   }
 
-  //  int tempHours = myRTC.hours;             //Get the hours and save to variable temp
-  //  hourTens = tempHours / 10;               //Split hours into two digits, tens and units
-  //  hourUnits = tempHours % 10;
-  //  int tempMins = myRTC.minutes;               //Get the minutes and save to variable temp
-  //  minuteTens = tempMins / 10;             //Split minutes into two digits, tens and units
-  //  minuteUnits = tempMins % 10;
-
-  //  if(minuteUnits != prevMinuteUnits)  //If minute units has changed, update display
-  //    updateDisplay();
+  //    if (minuteUnits != prevMinuteUnits) //If minute units has changed, update display
+  //      updateDisplay();
   //
-  //  prevHourTens = hourTens;            //Update previous displayed numerals
-  //  prevHourUnits = hourUnits;
-  //  prevMinuteTens = minuteTens;
-  //  prevMinuteUnits = minuteUnits;
-
-  delay(500);
+  //    prevHourTens = hourTens;            //Update previous displayed numerals
+  //    prevHourUnits = hourUnits;
+  //    prevMinuteTens = minuteTens;
+  //    prevMinuteUnits = minuteUnits;
+  //  }
 }
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
@@ -203,7 +202,87 @@ void packageDateTime(const RtcDateTime& dt)
              dt.Hour(),
              dt.Minute(),
              dt.Second() );
-//  Serial.print("Payload : ");
-//  Serial.print(datestring);
-//  Serial.println(CmdCode[2]);
+  //  Serial.print("Payload : ");
+  //  Serial.print(datestring);
+  //  Serial.println(CmdCode[2]);
+}
+
+void updateMid()                                              //Function to move the middle segements and adjacent ones out of the way
+{
+  if (digits[minuteUnits][6] != digits[prevMinuteUnits][6])   //Move adjacent segments for Minute units
+  {
+    if (digits[prevMinuteUnits][1] == 1)
+      pwmM.setPWM(1, 0, segmentMOn[1] - midOffset);
+    if (digits[prevMinuteUnits][6] == 1)
+      pwmM.setPWM(5, 0, segmentMOn[5] + midOffset);
+  }
+  delay(100);                                                 //Delay allows adjacent segments to move before moving middle
+  if (digits[minuteUnits][6] == 1)                            //Move Minute units middle segment if required
+    pwmM.setPWM(6, 0, segmentMOn[6]);
+  else
+    pwmM.setPWM(6, 0, segmentMOff[6]);
+  if (digits[minuteTens][6] != digits[prevMinuteTens][6])     //Move adjacent segments for Minute tens
+  {
+    if (digits[prevMinuteTens][1] == 1)
+      pwmM.setPWM(8, 0, segmentMOn[8] - midOffset);
+    if (digits[prevMinuteTens][6] == 1)
+      pwmM.setPWM(12, 0, segmentMOn[12] + midOffset);
+  }
+  delay(100);                                                 //Delay allows adjacent segments to move before moving middle
+  if (digits[minuteTens][6] == 1)                             //Move Minute tens middle segment if required
+    pwmM.setPWM(13, 0, segmentMOn[13]);
+  else
+    pwmM.setPWM(13, 0, segmentMOff[13]);
+  if (digits[hourUnits][6] != digits[prevHourUnits][6])       //Move adjacent segments for Hour units
+  {
+    if (digits[prevHourUnits][1] == 1)
+      pwmH.setPWM(1, 0, segmentHOn[1] - midOffset);
+    if (digits[prevHourUnits][6] == 1)
+      pwmH.setPWM(5, 0, segmentHOn[5] + midOffset);
+  }
+  delay(100);                                                 //Delay allows adjacent segments to move before moving middle
+  if (digits[hourUnits][6] == 1)                              //Move Hour units middle segment if required
+    pwmH.setPWM(6, 0, segmentHOn[6]);
+  else
+    pwmH.setPWM(6, 0, segmentHOff[6]);
+  if (digits[hourTens][6] != digits[prevHourTens][6])         //Move adjacent segments for Hour tens
+  {
+    if (digits[prevHourTens][1] == 1)
+      pwmH.setPWM(8, 0, segmentHOn[8] - midOffset);
+    if (digits[prevHourTens][6] == 1)
+      pwmH.setPWM(12, 0, segmentHOn[12] + midOffset);
+  }
+  delay(100);                                                 //Delay allows adjacent segments to move before moving middle
+  if (digits[hourTens][6] == 1)                               //Move Hour tens middle segment if required
+    pwmH.setPWM(13, 0, segmentHOn[13]);
+  else
+    pwmH.setPWM(13, 0, segmentHOff[13]);
+}
+
+void updateDisplay ()                               //Function to update the displayed time
+{
+  updateMid();                                      //Move the segments out of the way of the middle segment and then move the middle segments
+  for (int i = 0 ; i <= 5 ; i++)                    //Move the remaining segments
+  {
+    if (digits[hourTens][i] == 1)                   //Update the hour tens
+      pwmH.setPWM(i + 7, 0, segmentHOn[i + 7]);
+    else
+      pwmH.setPWM(i + 7, 0, segmentHOff[i + 7]);
+    delay(10);
+    if (digits[hourUnits][i] == 1)                  //Update the hour units
+      pwmH.setPWM(i, 0, segmentHOn[i]);
+    else
+      pwmH.setPWM(i, 0, segmentHOff[i]);
+    delay(10);
+    if (digits[minuteTens][i] == 1)                 //Update the minute tens
+      pwmM.setPWM(i + 7, 0, segmentMOn[i + 7]);
+    else
+      pwmM.setPWM(i + 7, 0, segmentMOff[i + 7]);
+    delay(10);
+    if (digits[minuteUnits][i] == 1)                //Update the minute units
+      pwmM.setPWM(i, 0, segmentMOn[i]);
+    else
+      pwmM.setPWM(i, 0, segmentMOff[i]);
+    delay(10);
+  }
 }
